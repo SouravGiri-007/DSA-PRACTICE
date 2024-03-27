5628a3dcac124a31ccd01f29e443ae09cdcdb016
Sourav Giri <souravgiri247@gmail.com>
2024-03-27 19:47:34 +0530

    Main Method

---
+++ Linear  Search/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] nums = {23,6,42,75,84,36,25,33,74,24,88};
+        int target = 74;
+        int element = linearseach(nums,target);
+        System.out.println(element);
+
+    }
+
+
+
+
+    static int linearseach(int [ ] arr, int target){
+        if (arr.length == 0){
+            return  -1;
+        }
+
+        for (int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                return  i;
+            }
+        }
+        return -1;
+    }
+
+    static int linearseach2(int [ ] arr, int target){
+        if (arr.length == 0){
+            return  -1;
+        }
+
+        for (int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                return  arr[i];
+            }
+        }
+        return -1;
+    }
+
+    static  boolean linearseach3(int [ ] arr, int target){
+        if (arr.length == 0){
+            return  false;
+        }
+
+        for (int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                return  true;
+            }
+        }
+        return false;
+    }
+}
