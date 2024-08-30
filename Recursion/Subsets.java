b56fd83652e9a04ab075a8fc21420cf05b89cad3
Sourav Giri <souravgiri247@gmail.com>
2024-08-31 01:57:37 +0530

    78.Subsets

---
+++ Recursion/Subsets.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author soura
+ */
+
+ // problem Link = "https://leetcode.com/problems/subsets/"
+public class Subsets {
+            public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+    
+    private void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+}
