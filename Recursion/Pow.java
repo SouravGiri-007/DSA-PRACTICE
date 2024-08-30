68bbd5a42535419af4fd0a5fa3fab48ea751bcaa
Sourav Giri <souravgiri247@gmail.com>
2024-08-31 01:42:13 +0530

    50. Pow(x, n)

---
+++ Recursion/Pow.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sourav
+ */
+
+//  problem link ="https://leetcode.com/problems/powx-n/"
+public class Pow {
+    public double myPow(double x, int n) {
+        
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        
+        double pow = 1;
+        
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        
+        return pow;
+    }
+}
