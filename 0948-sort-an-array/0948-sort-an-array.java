0cf69a0e7b634ac7905878e6cfe3fd603eab86e2
Sourav Giri <134923650+SouravGiri-007@users.noreply.github.com>
2025-01-08 22:48:57 +0530

    Time: 30 ms (36.39%), Space: 57.1 MB (22.21%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,48 +1,42 @@
-class Solution {
-    public int[] sortArray(int[] nums) {
+public class Solution {
+    public static int[] sortArray(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
         mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-    public void mergeSort(int[] nums, int left, int right) {
-        if(left >= right) return;
+
+    private static void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return; 
+        }
 
         int mid = left + (right - left) / 2;
-
         mergeSort(nums, left, mid);
         mergeSort(nums, mid + 1, right);
-
-        merge(nums, left, mid, right);
+        merge(nums, left, mid, right); 
     }
-    public void merge(int[] nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
 
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-
-        for(int i = 0; i < n1; i++) {
-            leftArray[i] = nums[left + i];
-        }
-        for(int j = 0; j < n2; j++) {
-            rightArray[j] = nums[mid + 1 + j];
-        }
-
-        int i = 0, j = 0;
-        int k = left;
-
-        while(i < n1 && j < n2) {
-            if(leftArray[i] <= rightArray[j]) {
-                nums[k++] = leftArray[i++];
+    private static void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
             } else {
-                nums[k++] = rightArray[j++];
+                temp[k++] = nums[j++];
             }
         }
 
-        while(i < n1) {
-            nums[k++] = leftArray[i++];
+        while (i <= mid) {
+            temp[k++] = nums[i++];
         }
-        while(j < n2) {
-            nums[k++] = rightArray[j++];
+
+        while (j <= right) {
+            temp[k++] = nums[j++];
         }
+
+        System.arraycopy(temp, 0, nums, left, temp.length);
     }
 }
